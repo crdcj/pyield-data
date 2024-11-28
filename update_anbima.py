a6f08b98bb4d95e6366e656570ee4037d6d99a87
Carlos Carvalho <cr.cj@outlook.com>
2024-11-28 20:07:02 -0300

    wip

---
+++ update_anbima.py
@@ -0,0 +1,30 @@
+import datetime as dt
+from zoneinfo import ZoneInfo
+
+import pandas as pd
+import pyield as yd
+
+# Configurações e constantes
+TIMEZONE_BZ = ZoneInfo("America/Sao_Paulo")
+ANBIMA_FILE = "anbima_data.parquet"
+
+
+def get_anbima_on_date(date: dt.date) -> pd.DataFrame:
+    keep_cols = ["BondType", "ReferenceDate", "MaturityDate", "IndicativeRate"]
+    return yd.anbima.data(reference_date=date)[keep_cols].copy()
+
+
+now_bz = dt.datetime.now(TIMEZONE_BZ)
+today_bz = now_bz.date()
+# Force a specific date for testing purposes
+# today_bz = pd.to_datetime("26-08-2024", dayfirst=True)
+
+if not yd.bday.is_business_day(today_bz):
+    raise ValueError("Today is not a business day.")
+
+(  # Load the ANBIMA data from the parquet file and update it with the new data
+    pd.concat([pd.read_parquet(ANBIMA_FILE), get_anbima_on_date(today_bz)])
+    .drop_duplicates(subset=["ReferenceDate", "BondType", "MaturityDate"], keep="last")
+    .reset_index(drop=True)
+    .to_parquet(ANBIMA_FILE, compression="gzip", index=False)
+)
---
+++ update_di.py
@@ -0,0 +1,34 @@
+import datetime as dt
+from zoneinfo import ZoneInfo
+
+import pandas as pd
+import pyield as yd
+
+# Configurações e constantes
+TIMEZONE_BZ = ZoneInfo("America/Sao_Paulo")
+DI_FILE = "di_data.parquet"
+
+
+def get_di_on_date(date: dt.date) -> pd.DataFrame:
+    df = yd.futures(contract_code="DI1", trade_date=date)
+    df.drop(columns=["DaysToExp"], inplace=True)
+    if "SettlementRate" not in df.columns:
+        raise ValueError("There is no Settlement data for today.")
+    return df
+
+
+now_bz = dt.datetime.now(TIMEZONE_BZ)
+today_bz = now_bz.date()
+# Force a specific date for testing purposes
+# today_bz = pd.to_datetime("26-08-2024", dayfirst=True)
+
+if not yd.bday.is_business_day(today_bz):
+    raise ValueError("Today is not a business day.")
+
+
+(  # Load the DI data from the parquet file and update it with the new data
+    pd.concat([pd.read_parquet(DI_FILE), get_di_on_date(today_bz)])
+    .drop_duplicates(subset=["TradeDate", "TickerSymbol"], keep="last")
+    .reset_index(drop=True)
+    .to_parquet(DI_FILE, compression="gzip", index=False)
+)
