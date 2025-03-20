bd1ed6a524f6223112dac13859268b2ab437ec5e
Carlos Carvalho <cr.cj@outlook.com>
2025-03-20 07:18:27 -0300

    snapshot

--- update.py
+++ update.py
@@ -7,12 +7,12 @@ import pyield as yd
 
 # Configurações e constantes
 BZ_TIMEZONE = ZoneInfo("America/Sao_Paulo")
-ANBIMA_FILE = "anbima_data.parquet"
-DI_FILE = "di_data.parquet"
+TPF_PARQUET = "anbima_data.parquet"
+TPF_PICKLE = "data/anbima_tpf.pkl.gz"
+DI_PARQUET = "di_data.parquet"
+DI_PICKLE = "data/di.pkl.gz"
 
-logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
-)
+logger = logging.getLogger(__name__)
 
 
 def get_di_on_date(date: dt.date) -> pd.DataFrame:
@@ -23,9 +23,52 @@ def get_di_on_date(date: dt.date) -> pd.DataFrame:
     return df
 
 
-def get_anbima_on_date(date: dt.date) -> pd.DataFrame:
-    keep_cols = ["BondType", "ReferenceDate", "MaturityDate", "IndicativeRate"]
+def get_tpf_on_date(date: dt.date) -> pd.DataFrame:
+    keep_cols = ["BondType", "ReferenceDate", "MaturityDate", "IndicativeRate", "Price"]
     return yd.anbima.tpf_data(date=date)[keep_cols].copy()
+
+
+def update_di_dataset(target_date: dt.date) -> None:
+    try:
+        df_old = pd.read_parquet(DI_PARQUET)
+        df_new = get_di_on_date(target_date)
+        df_updated = (
+            pd.concat([df_old, df_new])
+            .drop_duplicates(subset=["TradeDate", "TickerSymbol"], keep="last")
+            .sort_values(["TradeDate", "ExpirationDate"])
+            .reset_index(drop=True)
+        )
+
+        # Save to parquet and pickle
+        df_updated.to_parquet(DI_PARQUET, compression="gzip", index=False)
+        df_updated.to_pickle(DI_PICKLE, compression="gzip")
+
+        logger.info(f"DI dataset updated with data from {target_date}")
+
+    except Exception as e:
+        logger.error(f"Failed to update DI dataset: {e}")
+
+
+def update_tpf_dataset(target_date: dt.date) -> None:
+    try:
+        df_old = pd.read_parquet(TPF_PARQUET)
+        df_new = get_tpf_on_date(target_date)
+        key_cols = ["ReferenceDate", "BondType", "MaturityDate"]
+        df_updated = (
+            pd.concat([df_old, df_new])
+            .drop_duplicates(subset=key_cols, keep="last")
+            .sort_values(key_cols)
+            .reset_index(drop=True)
+        )
+
+        # Save to parquet and pickle
+        df_updated.to_parquet(TPF_PARQUET, compression="gzip", index=False)
+        df_updated.to_pickle(TPF_PICKLE, compression="gzip")
+
+        logger.info(f"ANBIMA dataset updated with data from {target_date}")
+
+    except Exception as e:
+        logger.error(f"Failed to update TPF dataset: {e}")
 
 
 def main():
@@ -36,41 +79,19 @@ def main():
     # target_date = pd.to_datetime("30-12-2024", dayfirst=True).date()
 
     if not yd.bday.is_business_day(target_date):
-        logging.warning("Target date is not a business day. Aborting...")
+        logger.warning("Target date is not a business day. Aborting...")
         return
 
     pre_xmas = dt.date(target_date.year, 12, 24)
     pre_ny = dt.date(target_date.year, 12, 31)
     if target_date == pre_xmas or target_date == pre_ny:
-        logging.warning(
+        logger.warning(
             "There is no session on the day before Christmas or New Year's Eve. Aborting..."
         )
         return
 
-    try:
-        (  # Load the DI data from the parquet file and update it with the new data
-            pd.concat([pd.read_parquet(DI_FILE), get_di_on_date(target_date)])
-            .drop_duplicates(subset=["TradeDate", "TickerSymbol"], keep="last")
-            .sort_values(["TradeDate", "ExpirationDate"])
-            .reset_index(drop=True)
-            .to_parquet(DI_FILE, compression="gzip", index=False)
-        )
-
-    except Exception as e:
-        logging.error(f"Failed to update DI dataset: {e}")
-
-    try:
-        subset_cols = ["ReferenceDate", "BondType", "MaturityDate"]
-        (  # Load the ANBIMA data from the parquet file and update it with the new data
-            pd.concat([pd.read_parquet(ANBIMA_FILE), get_anbima_on_date(target_date)])
-            .drop_duplicates(subset=subset_cols, keep="last")
-            .sort_values(subset_cols)
-            .reset_index(drop=True)
-            .to_parquet(ANBIMA_FILE, compression="gzip", index=False)
-        )
-
-    except Exception as e:
-        logging.error(f"Failed to update ANBIMA dataset: {e}")
+    update_di_dataset(target_date)
+    update_tpf_dataset(target_date)
 
 
 if __name__ == "__main__":
