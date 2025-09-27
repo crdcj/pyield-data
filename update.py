490fca9bd1e062062a524d84a9b3d7ec68c10b35
Carlos Carvalho <carlos.carvalho.jr@gmail.com>
2025-09-27 10:33:27 -0300

    snapshot

---
+++ update.py
@@ -0,0 +1,124 @@
+import datetime as dt
+import logging
+from pathlib import Path
+from zoneinfo import ZoneInfo
+
+import polars as pl
+import pyield as yd
+
+# Configurações e constantes
+BZ_TIMEZONE = ZoneInfo("America/Sao_Paulo")
+
+# Os arquivos estão na pasta data
+base_dir = Path(__file__).parent
+data_dir = base_dir / "data"
+
+DI_PARQUET = data_dir / "b3_di.parquet"
+TPF_PARQUET = data_dir / "anbima_tpf.parquet"
+
+
+logger = logging.getLogger(__name__)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
+
+
+def get_di1_on_date(date: dt.date) -> pl.DataFrame:
+    df_pd = yd.futures(contract_code="DI1", date=date)
+    df = pl.from_pandas(df_pd).drop("DaysToExp")
+    if "SettlementRate" not in df.columns:
+        raise ValueError("There is no Settlement data for today.")
+    return df
+
+
+def get_tpf_on_date(date: dt.date) -> pl.DataFrame:
+    df_pd = yd.anbima.tpf_data(date=date, fetch_from_source=True)
+    df = pl.from_pandas(df_pd)
+    selected_cols = [
+        "BondType",
+        "ReferenceDate",
+        "SelicCode",
+        "IssueBaseDate",
+        "MaturityDate",
+        "BDToMat",
+        "Duration",
+        "DV01",
+        "DV01USD",
+        "Price",
+        "BidRate",
+        "AskRate",
+        "IndicativeRate",
+        "DIRate",
+    ]
+    selected_cols = [col for col in selected_cols if col in df.columns]
+    return df.select(selected_cols)
+
+
+def update_di1_dataset(target_date: dt.date) -> None:
+    try:
+        df = pl.read_parquet(DI_PARQUET)
+        df_new = get_di1_on_date(target_date)
+        (
+            pl.concat([df, df_new], how="diagonal")
+            .unique(subset=["TradeDate", "TickerSymbol"], keep="last")
+            .sort(["TradeDate", "ExpirationDate"])
+            .write_parquet(DI_PARQUET, compression="gzip")
+        )
+
+        logger.info(f"DI dataset updated with data from {target_date}")
+    except Exception as e:
+        logger.error(f"Failed to update DI dataset: {e}")
+
+
+def update_tpf_dataset(target_date: dt.date) -> None:
+    try:
+        df = pl.read_parquet(TPF_PARQUET)
+        df_new = get_tpf_on_date(target_date)
+
+        key_cols = ["ReferenceDate", "BondType", "MaturityDate"]
+        (
+            pl.concat([df, df_new], how="diagonal")
+            .unique(subset=key_cols, keep="last")
+            .sort(key_cols)
+            .write_parquet(TPF_PARQUET, compression="gzip")
+        )
+
+        logger.info(f"TPF parquet updated with data from {target_date}")
+    except Exception as e:
+        logger.error(f"Failed to update TPF parquet: {e}")
+
+
+def main():
+    today = dt.datetime.now().date()
+    bz_today = dt.datetime.now(BZ_TIMEZONE).date()
+    if bz_today == today:
+        # Voltar um dia se as datas forem iguais
+        target_date = bz_today - dt.timedelta(days=1)  # Yesterday
+    else:
+        # Se as datas forem diferentes, Brasil está um dia atrás
+        # Logo já é a data correta
+        target_date = bz_today
+
+    # Force a specific date for testing purposes
+    # target_date = pd.to_datetime("26-08-2025", dayfirst=True).date()
+
+    if not yd.bday.is_business_day(target_date):
+        logger.warning("Target date is not a business day. Aborting...")
+        return
+
+    pre_xmas = dt.date(target_date.year, 12, 24)
+    pre_ny = dt.date(target_date.year, 12, 31)
+    if target_date == pre_xmas or target_date == pre_ny:
+        logger.warning(
+            "There is no session on the day before Christmas or New Year's Eve. Aborting..."
+        )
+        return
+
+    update_di1_dataset(target_date)
+    update_tpf_dataset(target_date)
+
+
+if __name__ == "__main__":
+    main()
