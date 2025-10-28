e4a9f49b659e0ce3fd4036468ca9c62054e28b15
Carlos Carvalho <carlos.carvalho.jr@gmail.com>
2025-10-28 08:52:12 -0300

    job corrigido

--- update.py
+++ update.py
@@ -13,7 +13,7 @@ BZ_TIMEZONE = ZoneInfo("America/Sao_Paulo")
 base_dir = Path(__file__).parent
 data_dir = base_dir / "data"
 
-DI_PARQUET = data_dir / "b3_di.parquet"
+DI1_PARQUET = data_dir / "b3_di.parquet"
 TPF_PARQUET = data_dir / "anbima_tpf.parquet"
 
 
@@ -60,54 +60,62 @@ def get_tpf_on_date(date: dt.date) -> pl.DataFrame:
 
 
 def update_di1_dataset(target_date: dt.date) -> None:
-    try:
-        df = pl.read_parquet(DI_PARQUET)
-        df_new = get_di1_on_date(target_date)
-        (
-            pl.concat([df, df_new], how="diagonal")
-            .unique(subset=["TradeDate", "TickerSymbol"], keep="last")
-            .sort(["TradeDate", "ExpirationDate"])
-            .write_parquet(DI_PARQUET, compression="gzip")
-        )
-
-        logger.info(f"DI dataset updated with data from {target_date}")
-    except Exception as e:
-        logger.error(f"Failed to update DI dataset: {e}")
+    df = pl.read_parquet(DI1_PARQUET)
+    df_new = get_di1_on_date(target_date)
+    (
+        pl.concat([df, df_new], how="diagonal")
+        .unique(subset=["TradeDate", "TickerSymbol"], keep="last")
+        .sort(["TradeDate", "ExpirationDate"])
+        .write_parquet(DI1_PARQUET, compression="gzip")
+    )
+    logger.info(f"DI dataset updated with data from {target_date}")
 
 
 def update_tpf_dataset(target_date: dt.date) -> None:
-    try:
-        df = pl.read_parquet(TPF_PARQUET)
-        df_new = get_tpf_on_date(target_date)
+    df = pl.read_parquet(TPF_PARQUET)
+    df_new = get_tpf_on_date(target_date)
 
-        key_cols = ["ReferenceDate", "BondType", "MaturityDate"]
-        (
-            pl.concat([df, df_new], how="diagonal")
-            .unique(subset=key_cols, keep="last")
-            .sort(key_cols)
-            .write_parquet(TPF_PARQUET, compression="gzip")
-        )
-
-        logger.info(f"TPF parquet updated with data from {target_date}")
-    except Exception as e:
-        logger.error(f"Failed to update TPF parquet: {e}")
+    key_cols = ["ReferenceDate", "BondType", "MaturityDate"]
+    (
+        pl.concat([df, df_new], how="diagonal")
+        .unique(subset=key_cols, keep="last")
+        .sort(key_cols)
+        .write_parquet(TPF_PARQUET, compression="gzip")
+    )
+    logger.info(f"TPF parquet updated with data from {target_date}")
 
 
 def main():
-    target_date = yd.bday.last_business_day()
+    now = dt.datetime.now(BZ_TIMEZONE)
+    today = now.date()
+
+    # Usando is_business_day torna a intenção mais explícita
+    if yd.bday.is_business_day(today):
+        # É dia útil, então o horário importa
+        if now.hour < 21:
+            trade_date = yd.bday.offset(today, -1)
+        else:
+            trade_date = today
+    else:
+        # Não é dia útil, então pegamos o último que existiu
+        trade_date = yd.bday.last_business_day(today)
+
+    logger.info(f"Determined target trade date: {trade_date}")
     # Force a specific date for testing purposes
     # target_date = dt.datetime.strptime("24-10-2025", "%d-%m-%Y").date()
 
-    pre_xmas = dt.date(target_date.year, 12, 24)
-    pre_ny = dt.date(target_date.year, 12, 31)
-    if target_date == pre_xmas or target_date == pre_ny:
-        logger.warning(
-            "There is no session on the day before Christmas or New Year's Eve. Aborting..."
-        )
-        return
+    pre_xmas = dt.date(trade_date.year, 12, 24)
+    pre_ny = dt.date(trade_date.year, 12, 31)
+    if trade_date in (pre_xmas, pre_ny):
+        logger.info("No trade updates on Christmas Eve or New Year's Eve.")
+        exit(0)
 
-    update_di1_dataset(target_date)
-    update_tpf_dataset(target_date)
+    try:
+        update_di1_dataset(trade_date)
+        update_tpf_dataset(trade_date)
+    except Exception as e:
+        logger.error(f"Failed to update datasets: {e}")
+        raise
 
 
 if __name__ == "__main__":
